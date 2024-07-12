95a124039132f30504783c0993db87de5aad9228
o-patrick <leme.patrick.contato@hotmail.com>
2024-07-12 15:59:37 -0300

    while and try... catch content added to t-sql folder

---
+++ tsql/try_catch.sql
@@ -0,0 +1,14 @@
+BEGIN TRY  
+    -- Testando divisão por zero
+    SELECT 1/1;  
+END TRY  
+BEGIN CATCH  
+    SELECT  
+        ERROR_NUMBER() AS ErrorNumber  
+        ,ERROR_SEVERITY() AS ErrorSeverity  
+        ,ERROR_STATE() AS ErrorState  
+        ,ERROR_PROCEDURE() AS ErrorProcedure  
+        ,ERROR_LINE() AS ErrorLine  
+        ,ERROR_MESSAGE() AS ErrorMessage;  
+END CATCH;  
+GO 
